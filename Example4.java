c855ec2004c4d580fd96081f5d0bb7baa91e0488
akerekon <89589162+akerekon@users.noreply.github.com>
2023-11-01 14:41:28 -0400

    Update Second Java Demo

--- Example4.java
+++ Example4.java
@@ -12,9 +12,15 @@ public class Example4 {
 
     //A (contrived) method to ensure all of the Boston Red Sox 2013 World Series roster is loaded in before a baseball game
     private void checkIfPlayersOnField() {
-        if (!firstBase.isEmpty() && !secondBase.isEmpty() && !shortstop.isEmpty()
-           && !thirdBase.isEmpty() && !leftField.isEmpty() && !centerField.isEmpty()
-           && !rightField.isEmpty() && !pitcher.isEmpty() && !catcher.isEmpty()) {
+        if (!firstBase.isEmpty()
+           && !secondBase.isEmpty()
+           && !shortstop.isEmpty()
+           && !thirdBase.isEmpty()
+           && !leftField.isEmpty()
+           && !centerField.isEmpty()
+           && !rightField.isEmpty()
+           && !pitcher.isEmpty()
+           && !catcher.isEmpty()) {
             System.out.println("All players are on the field!");
         } else {
             System.out.println("Player(s) are missing from the field!");
